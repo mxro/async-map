d80d01339ac8d495bf379b24ad16390a8dd38075
mxro <mxro@nowhere.com>
2014-08-12 09:20:19 +1000

    Reworking Nextweb persistence API and developing async-map abstraction Autocommit Tue Aug 12 09:20:19 EST 2014

--- src/main/java/de/mxro/async/map/jre/AsyncMapsJre.java
+++ src/main/java/de/mxro/async/map/jre/AsyncMapsJre.java
@@ -1,5 +1,17 @@
 package de.mxro.async.map.jre;
 
+import java.util.Collections;
+import java.util.WeakHashMap;
+
+import de.mxro.async.map.MapConnection;
+import de.mxro.async.map.internal.decorators.MapCacheMapConnection;
+
 public class AsyncMapsJre {
+
+	public static final MapConnection cacheWithWeakReferences(
+			MapConnection decorated) {
+		return new MapCacheMapConnection(Collections
+				.synchronizedMap(new WeakHashMap<String, Object>()), decorated);
+	}
 
 }

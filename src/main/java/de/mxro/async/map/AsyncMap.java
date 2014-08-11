05badd569c8942578712bbc5e13404cd357b1aa4
mxro <mxro@nowhere.com>
2014-08-12 09:01:01 +1000

    Reworking Nextweb persistence API and developing async-map abstraction Autocommit Tue Aug 12 09:01:01 EST 2014

--- src/main/java/de/mxro/async/map/AsyncMap.java
+++ src/main/java/de/mxro/async/map/AsyncMap.java
@@ -3,6 +3,16 @@ package de.mxro.async.map;
 import de.mxro.async.callbacks.SimpleCallback;
 import de.mxro.async.callbacks.ValueCallback;
 
+/**
+ * <p>
+ * A map for which values can be retrieved and set using an asynchronous
+ * interface.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ * @param <K>
+ * @param <V>
+ */
 public interface AsyncMap<K, V> {
 
 	public void put(K key, V value, SimpleCallback callback);

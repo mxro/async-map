8837944fb7e135e8eb26a2fa5ab77b974ba3599a
mxro <mxro@nowhere.com>
2014-08-12 08:46:46 +1000

    Reworking Nextweb persistence API and developing async-map abstraction Autocommit Tue Aug 12 08:46:46 EST 2014

--- src/main/java/de/mxro/async/map/operations/GetOperation.java
+++ src/main/java/de/mxro/async/map/operations/GetOperation.java
@@ -1,5 +1,25 @@
 package de.mxro.async.map.operations;
 
-public class GetOperation {
+import de.mxro.async.callbacks.ValueCallback;
+
+/**
+ * An object representation of a Get operation on an asynchronous map.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ * @param <T>
+ */
+public class GetOperation<T> {
+
+	private final ValueCallback<T> callback;
+
+	public ValueCallback<T> getCallback() {
+		return callback;
+	}
+
+	public GetOperation(ValueCallback<T> callback) {
+		super();
+		this.callback = callback;
+	}
 
 }

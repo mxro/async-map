b164b7466bc303fd16edd8244398ac9bb42c3bbe
mxro <mxro@nowhere.com>
2014-08-12 13:46:44 +1000

    a

---
+++ src/main/java/de/mxro/async/map/operations/ClearCacheOperation.java
@@ -0,0 +1,12 @@
+package de.mxro.async.map.operations;
+
+/**
+ * If the persistence implementation supports a local cache, calling this method
+ * clears this cache.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
+public class ClearCacheOperation {
+
+}

369b2fce4e966101c9b081302acfa3b70ef92659
mxro <mxro@nowhere.com>
2014-08-12 12:20:31 +1000

    Reworking Nextweb persistence connection and implementing asyc-map framework Autocommit Tue Aug 12 12:20:31 EST 2014

--- src/main/java/de/mxro/async/map/jre/AsyncMapsJre.java
+++ src/main/java/de/mxro/async/map/jre/AsyncMapsJre.java
@@ -3,21 +3,24 @@ package de.mxro.async.map.jre;
 import java.util.Collections;
 import java.util.WeakHashMap;
 
-import de.mxro.async.map.MapConnection;
+import de.mxro.async.map.AsyncMap;
+import de.mxro.async.map.PersistedMap;
 import de.mxro.async.map.internal.decorators.MapCacheMapConnection;
 import de.mxro.async.map.jre.internal.SplitWorkerThreadsMapConnection;
 
 public class AsyncMapsJre {
 
-	public static final MapConnection cacheWithWeakReferences(
-			MapConnection decorated) {
-		return new MapCacheMapConnection(Collections
-				.synchronizedMap(new WeakHashMap<String, Object>()), decorated);
+	public static final <K, V> AsyncMap<K, V> cacheWithWeakReferences(
+			AsyncMap<K, V> decorated) {
+		return new MapCacheMapConnection<K, V>(
+				Collections.synchronizedMap(new WeakHashMap<K, Object>()),
+				decorated);
 	}
 
-	public static final MapConnection divideWork(int workerThreads,
-			MapConnection decorated) {
-		return new SplitWorkerThreadsMapConnection(decorated, workerThreads);
+	public static final <K, V> PersistedMap<K, V> divideWork(int workerThreads,
+			PersistedMap<K, V> decorated) {
+		return new SplitWorkerThreadsMapConnection<K, V>(decorated,
+				workerThreads);
 	}
 
 }

40c92acc7997146c02823971b52e3790f2aefc6c
mxro <mxro@nowhere.com>
2014-08-13 13:05:21 +1000

    Writing tests for SQL implementation of async map and fixing issues Autocommit Wed Aug 13 13:05:21 EST 2014

--- src/main/java/de/mxro/async/map/AsyncMaps.java
+++ src/main/java/de/mxro/async/map/AsyncMaps.java
@@ -3,10 +3,12 @@ package de.mxro.async.map;
 import java.util.Map;
 
 import de.mxro.async.map.internal.decorators.DelayPutConnection;
+import de.mxro.async.map.internal.decorators.KeyFilterMap;
 import de.mxro.async.map.internal.decorators.LazyStartupMap;
 import de.mxro.async.map.internal.decorators.MapCacheMapConnection;
 import de.mxro.async.map.internal.decorators.PurgeInvalidValuesMap;
 import de.mxro.concurrency.Concurrency;
+import de.mxro.fn.Function;
 
 public class AsyncMaps {
 
@@ -25,13 +27,18 @@ public class AsyncMaps {
 		return new MapCacheMapConnection<K, V>(cache, decorated);
 	}
 
+	public final static <K, V> AsyncMap<K, V> filterKeys(Function<K, K> filter,
+			AsyncMap<K, V> decorated) {
+		return new KeyFilterMap<K, V>(filter, decorated);
+	}
+
 	/**
 	 * 
 	 * @param decorated
 	 * @return
 	 * @see LazyStartupMap
 	 */
-	public static <K,V> AsyncMap<K,V> lazyStartup(AsyncMap<K,V> decorated) {
+	public static <K, V> AsyncMap<K, V> lazyStartup(AsyncMap<K, V> decorated) {
 		return new LazyStartupMap<K, V>(decorated);
 	}
 }

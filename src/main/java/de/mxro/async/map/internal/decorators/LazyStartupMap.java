f38313a8c73cb04643cb9802ffb02fda5cdbec85
mxro <mxro@nowhere.com>
2014-09-06 14:27:29 +1000

    Fixing issue that persisted map is not started up correcty for js client Autocommit Sat Sep 06 14:27:29 EST 2014

--- src/main/java/de/mxro/async/map/internal/decorators/LazyStartupMap.java
+++ src/main/java/de/mxro/async/map/internal/decorators/LazyStartupMap.java
@@ -23,277 +23,271 @@ import de.mxro.async.map.operations.MapOperation;
  */
 class LazyStartupMap<K, V> implements AsyncMap<K, V> {
 
-	private static final String ERROR_MESSAGE = "Lazy start map is not started up. Needs to be started using a call to start or a call to one of its asynchronous operations.";
-	final Value<Boolean> started;
-	final AsyncMap<K, V> decorated;
-	final List<SimpleCallback> starting;
-	/**
-	 * An unsynchronized variable, which allows faster access to the started
-	 * state.
-	 */
-	boolean started_fast_access;
+    private static final String ERROR_MESSAGE = "Lazy start map is not started up. Needs to be started using a call to start or a call to one of its asynchronous operations.";
+    final Value<Boolean> started;
+    final AsyncMap<K, V> decorated;
+    final List<SimpleCallback> starting;
+    /**
+     * An unsynchronized variable, which allows faster access to the started
+     * state.
+     */
+    boolean started_fast_access;
 
-	@Override
-	public void put(final K key, final V value, final SimpleCallback callback) {
-		if (started_fast_access) {
-			decorated.put(key, value, callback);
-			return;
-		}
+    @Override
+    public void put(final K key, final V value, final SimpleCallback callback) {
+        if (started_fast_access) {
+            decorated.put(key, value, callback);
+            return;
+        }
 
-		synchronized (started) {
-			if (started.get()) {
-				decorated.put(key, value, callback);
-				return;
-			}
+        synchronized (started) {
+            if (started.get()) {
+                decorated.put(key, value, callback);
+                return;
+            }
 
-			start(new SimpleCallback() {
+            start(new SimpleCallback() {
 
-				@Override
-				public void onFailure(Throwable t) {
-					callback.onFailure(new Exception(
-							"Delayed start could not be performed.", t));
-				}
+                @Override
+                public void onFailure(final Throwable t) {
+                    callback.onFailure(new Exception("Delayed start could not be performed.", t));
+                }
 
-				@Override
-				public void onSuccess() {
-					decorated.put(key, value, callback);
-				}
-			});
-		}
+                @Override
+                public void onSuccess() {
+                    decorated.put(key, value, callback);
+                }
+            });
+        }
 
-	}
+    }
 
-	@Override
-	public void get(final K key, final ValueCallback<V> callback) {
-		if (started_fast_access) {
-			decorated.get(key, callback);
-			return;
-		}
+    @Override
+    public void get(final K key, final ValueCallback<V> callback) {
+        if (started_fast_access) {
+            decorated.get(key, callback);
+            return;
+        }
 
-		synchronized (started) {
-			if (started.get()) {
-				decorated.get(key, callback);
-				return;
-			}
+        synchronized (started) {
+            if (started.get()) {
+                decorated.get(key, callback);
+                return;
+            }
 
-			start(new SimpleCallback() {
+            start(new SimpleCallback() {
 
-				@Override
-				public void onFailure(Throwable t) {
-					callback.onFailure(new Exception(
-							"Delayed start could not be performed.", t));
-				}
+                @Override
+                public void onFailure(final Throwable t) {
+                    callback.onFailure(new Exception("Delayed start could not be performed.", t));
+                }
 
-				@Override
-				public void onSuccess() {
-					decorated.get(key, callback);
-				}
-			});
-		}
+                @Override
+                public void onSuccess() {
+                    decorated.get(key, callback);
+                }
+            });
+        }
 
-	}
+    }
 
-	@Override
-	public void remove(final K key, final SimpleCallback callback) {
-		if (started_fast_access) {
-			decorated.remove(key, callback);
-			return;
-		}
+    @Override
+    public void remove(final K key, final SimpleCallback callback) {
+        if (started_fast_access) {
+            decorated.remove(key, callback);
+            return;
+        }
 
-		synchronized (started) {
-			if (started.get()) {
-				decorated.remove(key, callback);
-				return;
-			}
+        synchronized (started) {
+            if (started.get()) {
+                decorated.remove(key, callback);
+                return;
+            }
 
-			start(new SimpleCallback() {
+            start(new SimpleCallback() {
 
-				@Override
-				public void onFailure(Throwable t) {
-					callback.onFailure(new Exception(
-							"Delayed start could not be performed.", t));
-				}
+                @Override
+                public void onFailure(final Throwable t) {
+                    callback.onFailure(new Exception("Delayed start could not be performed.", t));
+                }
 
-				@Override
-				public void onSuccess() {
-					decorated.remove(key, callback);
-				}
-			});
-		}
+                @Override
+                public void onSuccess() {
+                    decorated.remove(key, callback);
+                }
+            });
+        }
 
-	}
+    }
 
-	@Override
-	public V getSync(K key) {
-		if (started_fast_access) {
+    @Override
+    public V getSync(final K key) {
+        if (started_fast_access) {
 
-			return decorated.getSync(key);
-		}
+            return decorated.getSync(key);
+        }
 
-		synchronized (started) {
-			if (started.get()) {
+        synchronized (started) {
+            if (started.get()) {
 
-				return decorated.getSync(key);
-			}
-		}
+                return decorated.getSync(key);
+            }
+        }
 
-		throw new RuntimeException(
-				ERROR_MESSAGE);
+        throw new RuntimeException(ERROR_MESSAGE);
 
-	}
+    }
 
-	@Override
-	public void putSync(K key, V value) {
-		if (started_fast_access) {
-			decorated.putSync(key, value);
-			return;
-		}
+    @Override
+    public void putSync(final K key, final V value) {
+        if (started_fast_access) {
+            decorated.putSync(key, value);
+            return;
+        }
 
-		synchronized (started) {
-			if (started.get()) {
-				decorated.putSync(key, value);
-				return;
-			}
-		}
+        synchronized (started) {
+            if (started.get()) {
+                decorated.putSync(key, value);
+                return;
+            }
+        }
 
-		throw new RuntimeException(
-				ERROR_MESSAGE);
+        throw new RuntimeException(ERROR_MESSAGE);
 
-	}
+    }
 
-	@Override
-	public void removeSync(K key) {
-		if (started_fast_access) {
-			decorated.removeSync(key);
-			return;
-		}
+    @Override
+    public void removeSync(final K key) {
+        if (started_fast_access) {
+            decorated.removeSync(key);
+            return;
+        }
 
-		synchronized (started) {
-			if (started.get()) {
-				decorated.removeSync(key);
-				return;
-			}
-		}
+        synchronized (started) {
+            if (started.get()) {
+                decorated.removeSync(key);
+                return;
+            }
+        }
 
-		throw new RuntimeException(
-				ERROR_MESSAGE);
+        throw new RuntimeException(ERROR_MESSAGE);
 
-	}
+    }
 
-	@Override
-	public void start(SimpleCallback callback) {
-		synchronized (starting) {
-			if (starting.size() > 0) {
-				starting.add(callback);
-				return;
-			}
-		}
-		starting.add(callback);
-		decorated.start(new SimpleCallback() {
-			
-			@Override
-			public void onFailure(Throwable t) {
-				final ArrayList<SimpleCallback> startingCopy;
-				synchronized (starting) {
-					startingCopy = new ArrayList<SimpleCallback>(starting);
-					starting.clear();
-				}
-				
-				for (SimpleCallback callback: startingCopy) {
-					callback.onFailure(t);
-				}
-			}
-			
-			@Override
-			public void onSuccess() {
-				synchronized (started) {
-					started_fast_access = true;
-					started.set(true);
-				}
-				final ArrayList<SimpleCallback> startingCopy;
-				synchronized (starting) {
-					startingCopy = new ArrayList<SimpleCallback>(starting);
-					starting.clear();
-				}
-				
-				for (SimpleCallback callback: startingCopy) {
-					callback.onSuccess();
-				}
-			}
-		});
-		
-	}
+    @Override
+    public void start(final SimpleCallback callback) {
+        synchronized (starting) {
+            if (starting.size() > 0) {
+                starting.add(callback);
+                return;
+            }
+        }
+        starting.add(callback);
+        decorated.start(new SimpleCallback() {
 
-	@Override
-	public void stop(SimpleCallback callback) {
-		if (started_fast_access) {
-			decorated.stop(callback);
-			return;
-		}
+            @Override
+            public void onFailure(final Throwable t) {
+                final ArrayList<SimpleCallback> startingCopy;
+                synchronized (starting) {
+                    startingCopy = new ArrayList<SimpleCallback>(starting);
+                    starting.clear();
+                }
 
-		synchronized (started) {
-			if (started.get()) {
-				decorated.stop(callback);
-				return;
-			}
-		}
+                for (final SimpleCallback callback : startingCopy) {
+                    callback.onFailure(t);
+                }
+            }
 
-		callback.onSuccess();
-	}
+            @Override
+            public void onSuccess() {
+                synchronized (started) {
+                    started_fast_access = true;
+                    started.set(true);
+                }
 
-	@Override
-	public void commit(final SimpleCallback callback) {
-		if (started_fast_access) {
-			decorated.commit(callback);
-			return;
-		}
+                final ArrayList<SimpleCallback> startingCopy;
+                synchronized (starting) {
+                    startingCopy = new ArrayList<SimpleCallback>(starting);
+                    starting.clear();
+                }
 
-		synchronized (started) {
-			if (started.get()) {
-				decorated.commit(callback);
-				return;
-			}
-		}
-		
-		start(new SimpleCallback() {
-			
-			@Override
-			public void onFailure(Throwable t) {
-				callback.onFailure(t);
-			}
-			
-			@Override
-			public void onSuccess() {
-				decorated.commit(callback);
-			}
-		});
-	}
+                for (final SimpleCallback callback : startingCopy) {
+                    callback.onSuccess();
+                }
+            }
+        });
 
-	@Override
-	public void performOperation(MapOperation operation) {
-		if (started_fast_access) {
-			decorated.performOperation(operation);
-			return;
-		}
+    }
 
-		synchronized (started) {
-			if (started.get()) {
-				decorated.performOperation(operation);
-				return;
-			}
-		}
+    @Override
+    public void stop(final SimpleCallback callback) {
+        if (started_fast_access) {
+            decorated.stop(callback);
+            return;
+        }
 
-		throw new RuntimeException(
-				ERROR_MESSAGE);
+        synchronized (started) {
+            if (started.get()) {
+                decorated.stop(callback);
+                return;
+            }
+        }
 
-	}
+        callback.onSuccess();
+    }
 
-	public LazyStartupMap(AsyncMap<K, V> decorated) {
-		super();
-		this.started_fast_access = false;
-		this.started = new Value<Boolean>(false);
-		this.decorated = decorated;
-		this.starting = new LinkedList<SimpleCallback>();
-	}
+    @Override
+    public void commit(final SimpleCallback callback) {
+        if (started_fast_access) {
+            decorated.commit(callback);
+            return;
+        }
+
+        synchronized (started) {
+            if (started.get()) {
+                decorated.commit(callback);
+                return;
+            }
+        }
+
+        start(new SimpleCallback() {
+
+            @Override
+            public void onFailure(final Throwable t) {
+                callback.onFailure(t);
+            }
+
+            @Override
+            public void onSuccess() {
+                decorated.commit(callback);
+            }
+        });
+    }
+
+    @Override
+    public void performOperation(final MapOperation operation) {
+        if (started_fast_access) {
+            decorated.performOperation(operation);
+            return;
+        }
+
+        synchronized (started) {
+            if (started.get()) {
+                decorated.performOperation(operation);
+                return;
+            }
+        }
+
+        throw new RuntimeException(ERROR_MESSAGE);
+
+    }
+
+    public LazyStartupMap(final AsyncMap<K, V> decorated) {
+        super();
+        this.started_fast_access = false;
+        this.started = new Value<Boolean>(false);
+        this.decorated = decorated;
+        this.starting = new LinkedList<SimpleCallback>();
+    }
 
 }

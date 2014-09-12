1665785f0169336b924224895275e82976510fad
mxro <mxro@nowhere.com>
2014-09-12 13:58:41 +1000

    Fixing obfustication Autocommit Fri Sep 12 13:58:41 EST 2014

--- src/main/java/de/mxro/async/map/jre/internal/SplitWorkerThreadsMapConnection.java
+++ src/main/java/de/mxro/async/map/jre/internal/SplitWorkerThreadsMapConnection.java
@@ -14,268 +14,258 @@ import de.mxro.async.map.operations.MapOperation;
 import de.mxro.fn.Fn;
 import de.mxro.fn.Success;
 
-public class SplitWorkerThreadsMapConnection<K, V> implements AsyncMap<K, V> {
+public final class SplitWorkerThreadsMapConnection<K, V> implements AsyncMap<K, V> {
 
-	private final AsyncMap<K,V> decorated;
-	private ExecutorService executor;
-	private ConcurrentHashMap<K, Object> pendingPuts;
+    private final AsyncMap<K, V> decorated;
+    private ExecutorService executor;
+    private ConcurrentHashMap<K, Object> pendingPuts;
 
-	private final static Object NULL = Fn.object();
+    private final static Object NULL = Fn.object();
 
-	@SuppressWarnings("unchecked")
-	private final void writeValue(final K key, final SimpleCallback callback) {
-		synchronized (this) {
-			Object value = pendingPuts.get(key);
-			if (value == null) {
-				callback.onSuccess();
-				return;
-			}
-			
-			if (!pendingPuts.remove(key, value)) {
-				// do nothing, someone has inserted an updated value.
-				
-				//throw new IllegalStateException(
-				//		"Inserted key could not be removed. Key: "+key+" Value: "+value);
-			}
-			
-			if (value != NULL) {
-				decorated.put(key, (V) value, callback);
-			} else {
-				decorated.remove(key, new SimpleCallback() {
+    @SuppressWarnings("unchecked")
+    private final void writeValue(final K key, final SimpleCallback callback) {
+        synchronized (this) {
+            final Object value = pendingPuts.get(key);
+            if (value == null) {
+                callback.onSuccess();
+                return;
+            }
 
-					@Override
-					public void onFailure(Throwable t) {
-						callback.onFailure(t);
-					}
+            if (!pendingPuts.remove(key, value)) {
+                // do nothing, someone has inserted an updated value.
 
-					@Override
-					public void onSuccess() {
-						callback.onSuccess();
-					}
-				});
-			}
-			
-		}
+                // throw new IllegalStateException(
+                // "Inserted key could not be removed. Key: "+key+" Value: "+value);
+            }
 
-	}
+            if (value != NULL) {
+                decorated.put(key, (V) value, callback);
+            } else {
+                decorated.remove(key, new SimpleCallback() {
 
-	@Override
-	public void put(final K key, final V value,
-			final SimpleCallback callback) {
-		
-		// System.out.println("put "+key);
-		if (value != null) {
-			pendingPuts.put(key, value);
-		} else {
-			pendingPuts.put(key, NULL);
-		}
+                    @Override
+                    public void onFailure(final Throwable t) {
+                        callback.onFailure(t);
+                    }
 
-		executor.execute(new Runnable() {
+                    @Override
+                    public void onSuccess() {
+                        callback.onSuccess();
+                    }
+                });
+            }
 
-			@Override
-			public void run() {
-				writeValue(key, callback);
-			}
-		});
-	}
+        }
 
-	@Override
-	public void putSync(final K key, final V value) {
-		AsyncJre.waitFor(new Deferred<Success>() {
+    }
 
-			@Override
-			public void get(final ValueCallback<Success> callback) {
-				put(key, value, new SimpleCallback() {
-					
-					@Override
-					public void onFailure(Throwable t) {
-						callback.onFailure(t);
-					}
-					
-					@Override
-					public void onSuccess() {
-						callback.onSuccess(Success.INSTANCE);
-					}
-				});
-			}
-		});
-		
-	}
+    @Override
+    public void put(final K key, final V value, final SimpleCallback callback) {
 
-	@Override
-	public void removeSync(final K key) {
-		AsyncJre.waitFor(new Deferred<Success>() {
+        // System.out.println("put "+key);
+        if (value != null) {
+            pendingPuts.put(key, value);
+        } else {
+            pendingPuts.put(key, NULL);
+        }
 
-			@Override
-			public void get(final ValueCallback<Success> callback) {
-				remove(key, new SimpleCallback() {
-					
-					@Override
-					public void onFailure(Throwable t) {
-						callback.onFailure(t);
-					}
-					
-					@Override
-					public void onSuccess() {
-						callback.onSuccess(Success.INSTANCE);
-					}
-				});
-			}
-		});
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public void get(K key, ValueCallback<V> callback) {
-		Object value = pendingPuts.get(key);
+        executor.execute(new Runnable() {
 
-		if (value != null) {
-			if (value != NULL) {
-				callback.onSuccess((V) value);
-				return;
-			} else {
-				callback.onSuccess(null);
-				return;
-			}
-		}
+            @Override
+            public void run() {
+                writeValue(key, callback);
+            }
+        });
+    }
 
-		decorated.get(key, callback);
-	}
+    @Override
+    public void putSync(final K key, final V value) {
+        AsyncJre.waitFor(new Deferred<Success>() {
 
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public V getSync(K key) {
-		
-		Object value = pendingPuts.get(key);
+            @Override
+            public void get(final ValueCallback<Success> callback) {
+                put(key, value, new SimpleCallback() {
 
-		if (value != null) {
-			if (value != NULL) {
-				return (V) value;
-			} else {
-				return null;
-			}
-		}
+                    @Override
+                    public void onFailure(final Throwable t) {
+                        callback.onFailure(t);
+                    }
 
-		return decorated.getSync(key);
-	}
+                    @Override
+                    public void onSuccess() {
+                        callback.onSuccess(Success.INSTANCE);
+                    }
+                });
+            }
+        });
 
-	@Override
-	public void remove(final K key, final SimpleCallback callback) {
-		pendingPuts.put(key, NULL);
-		executor.execute(new Runnable() {
+    }
 
-			@Override
-			public void run() {
-				writeValue(key, new SimpleCallback() {
+    @Override
+    public void removeSync(final K key) {
+        AsyncJre.waitFor(new Deferred<Success>() {
 
-					@Override
-					public void onFailure(Throwable t) {
-						callback.onFailure(t);
-					}
+            @Override
+            public void get(final ValueCallback<Success> callback) {
+                remove(key, new SimpleCallback() {
 
-					@Override
-					public void onSuccess() {
-						callback.onSuccess();
-					}
-				});
-			}
-		});
-	}
+                    @Override
+                    public void onFailure(final Throwable t) {
+                        callback.onFailure(t);
+                    }
 
-	@Override
-	public void stop(final SimpleCallback callback) {
+                    @Override
+                    public void onSuccess() {
+                        callback.onSuccess(Success.INSTANCE);
+                    }
+                });
+            }
+        });
+    }
 
-		commit(new SimpleCallback() {
+    @SuppressWarnings("unchecked")
+    @Override
+    public void get(final K key, final ValueCallback<V> callback) {
+        final Object value = pendingPuts.get(key);
 
-			@Override
-			public void onFailure(Throwable t) {
-				callback.onFailure(t);
-			}
+        if (value != null) {
+            if (value != NULL) {
+                callback.onSuccess((V) value);
+                return;
+            } else {
+                callback.onSuccess(null);
+                return;
+            }
+        }
 
-			@Override
-			public void onSuccess() {
-				decorated.stop(new SimpleCallback() {
+        decorated.get(key, callback);
+    }
 
-					@Override
-					public void onFailure(Throwable t) {
-						callback.onFailure(t);
-					}
+    @SuppressWarnings("unchecked")
+    @Override
+    public V getSync(final K key) {
 
-					@Override
-					public void onSuccess() {
-						new Thread() {
+        final Object value = pendingPuts.get(key);
 
-							@Override
-							public void run() {
-								executor.shutdown();
+        if (value != null) {
+            if (value != NULL) {
+                return (V) value;
+            } else {
+                return null;
+            }
+        }
 
-								try {
-									executor.awaitTermination(60000,
-											TimeUnit.MILLISECONDS);
-								} catch (InterruptedException e) {
-									callback.onFailure(e);
-									return;
-								}
-								callback.onSuccess();
+        return decorated.getSync(key);
+    }
 
-							}
+    @Override
+    public void remove(final K key, final SimpleCallback callback) {
+        pendingPuts.put(key, NULL);
+        executor.execute(new Runnable() {
 
-						}.start();
-					}
-				});
-			}
-		});
+            @Override
+            public void run() {
+                writeValue(key, new SimpleCallback() {
 
-	}
+                    @Override
+                    public void onFailure(final Throwable t) {
+                        callback.onFailure(t);
+                    }
 
-	
-	
+                    @Override
+                    public void onSuccess() {
+                        callback.onSuccess();
+                    }
+                });
+            }
+        });
+    }
 
+    @Override
+    public void stop(final SimpleCallback callback) {
 
-	@Override
-	public void start(final SimpleCallback callback) {
-		this.executor = Executors.newFixedThreadPool(4);
-		this.pendingPuts = new ConcurrentHashMap<K, Object>();
-		callback.onSuccess();
-	}
+        commit(new SimpleCallback() {
 
-	@Override
-	public void commit(final SimpleCallback callback) {
+            @Override
+            public void onFailure(final Throwable t) {
+                callback.onFailure(t);
+            }
 
-		new Thread() {
+            @Override
+            public void onSuccess() {
+                decorated.stop(new SimpleCallback() {
 
-			@Override
-			public void run() {
+                    @Override
+                    public void onFailure(final Throwable t) {
+                        callback.onFailure(t);
+                    }
 
-				while (pendingPuts.size() > 0) {
-					try {
-						Thread.sleep(10);
-					} catch (InterruptedException e) {
-						callback.onFailure(e);
-						return;
-					}
-				}
+                    @Override
+                    public void onSuccess() {
+                        new Thread() {
 
-				decorated.commit(callback);
-			}
+                            @Override
+                            public void run() {
+                                executor.shutdown();
 
-		}.start();
+                                try {
+                                    executor.awaitTermination(60000, TimeUnit.MILLISECONDS);
+                                } catch (final InterruptedException e) {
+                                    callback.onFailure(e);
+                                    return;
+                                }
+                                callback.onSuccess();
 
-	}
+                            }
 
-	
+                        }.start();
+                    }
+                });
+            }
+        });
 
-	@Override
-	public void performOperation(MapOperation operation) {
-		decorated.performOperation(operation);
-	}
+    }
 
-	public SplitWorkerThreadsMapConnection(AsyncMap<K,V> connection,
-			int workerThreads) {
-		super();
-		this.decorated = connection;
-		
-	}
+    @Override
+    public void start(final SimpleCallback callback) {
+        this.executor = Executors.newFixedThreadPool(4);
+        this.pendingPuts = new ConcurrentHashMap<K, Object>();
+        callback.onSuccess();
+    }
+
+    @Override
+    public void commit(final SimpleCallback callback) {
+
+        new Thread() {
+
+            @Override
+            public void run() {
+
+                while (pendingPuts.size() > 0) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (final InterruptedException e) {
+                        callback.onFailure(e);
+                        return;
+                    }
+                }
+
+                decorated.commit(callback);
+            }
+
+        }.start();
+
+    }
+
+    @Override
+    public void performOperation(final MapOperation operation) {
+        decorated.performOperation(operation);
+    }
+
+    public SplitWorkerThreadsMapConnection(final AsyncMap<K, V> connection, final int workerThreads) {
+        super();
+        this.decorated = connection;
+
+    }
 
 }

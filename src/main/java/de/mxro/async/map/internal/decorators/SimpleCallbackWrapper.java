6e7c2fad8b66c86dc15d4c18a1584065884043b1
mxro <mxro@nowhere.com>
2014-09-15 08:12:30 +1000

    Trying to fix double obfustication bug. -> Simplifying classes causing problems Autocommit Mon Sep 15 08:12:30 EST 2014

--- src/main/java/de/mxro/async/map/internal/decorators/SimpleCallbackWrapper.java
+++ src/main/java/de/mxro/async/map/internal/decorators/SimpleCallbackWrapper.java
@@ -3,8 +3,15 @@ package de.mxro.async.map.internal.decorators;
 import de.mxro.async.callbacks.SimpleCallback;
 
 /**
+ * <p>
  * For some reason, proguard does not obfusticate the SimpleCallback anonymous
- * classes in the LazyStartup Map. That's why I use this wrapper class.
+ * classes in the LazyStartup Map and EnforceAsynchronousPutMap.
+ * <p>
+ * Using this class instead of the interface SimpleCallback to instantiate the
+ * anonoymous classes seems to solve the problem.
+ * <p>
+ * As a side effect, this also allows for more compact minimized code when
+ * SimpleCallback is excluded from being obfusticated.
  * 
  * @author <a href="http://www.mxro.de">Max Rohde</a>
  *

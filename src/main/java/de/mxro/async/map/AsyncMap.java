8f1c96e5051ed06f2873e513c10e6c69948a67ec
mxro <mxro@nowhere.com>
2014-12-31 10:21:42 +1100

    Make Local Cache API Asynchronous Autocommit Wed Dec 31 10:21:42 EST 2014

--- src/main/java/de/mxro/async/map/AsyncMap.java
+++ src/main/java/de/mxro/async/map/AsyncMap.java
@@ -8,7 +8,8 @@ import de.mxro.async.map.operations.MapOperation;
 import de.mxro.service.Service;
 
 /**
- * <p>An interface for an asynchronous, persisted map.
+ * <p>
+ * An interface for an asynchronous, persisted map.
  * 
  * @author <a href="http://www.mxro.de">Max Rohde</a>
  *
@@ -17,50 +18,51 @@ import de.mxro.service.Service;
  */
 public interface AsyncMap<K, V> extends Service {
 
-	
-	public void put(K key, V value, SimpleCallback callback);
+    public void put(K key, V value, SimpleCallback callback);
 
-	public void get(K key, ValueCallback<V> callback);
+    public void get(K key, ValueCallback<V> callback);
 
-	public void remove(K key, SimpleCallback callback);
+    public void remove(K key, SimpleCallback callback);
 
-	public V getSync(K key);
+    public V getSync(K key);
 
-	public void putSync(K key, V value);
+    public void putSync(K key, V value);
 
-	public void removeSync(K key);
+    public void removeSync(K key);
 
-	/**
-	 * Assures that a connection to the persistence medium is established and
-	 * the map is operational.
-	 * 
-	 * @param callback
-	 */
-	public void start(SimpleCallback callback);
+    /**
+     * Assures that a connection to the persistence medium is established and
+     * the map is operational.
+     * 
+     * @param callback
+     */
+    @Override
+    public void start(SimpleCallback callback);
 
-	/**
-	 * <p>
-	 * Releases resources held by this map.
-	 * <p>
-	 * Assures pending changes are written to persistence medium.
-	 * 
-	 * @param callback
-	 */
-	public void stop(SimpleCallback callback);
+    /**
+     * <p>
+     * Releases resources held by this map.
+     * <p>
+     * Assures pending changes are written to persistence medium.
+     * 
+     * @param callback
+     */
+    @Override
+    public void stop(SimpleCallback callback);
 
-	/**
-	 * Assures pending changes are written to persistence medium.
-	 * 
-	 * @param callback
-	 */
-	public void commit(SimpleCallback callback);
+    /**
+     * Assures pending changes are written to persistence medium.
+     * 
+     * @param callback
+     */
+    public void commit(SimpleCallback callback);
 
-	/**
-	 * <p>
-	 * Perform a generic operation on this map.
-	 * <p>
-	 * For example, {@link ClearCacheOperation}, {@link GetOperation}
-	 */
-	public void performOperation(MapOperation operation);
+    /**
+     * <p>
+     * Perform a generic operation on this map.
+     * <p>
+     * For example, {@link ClearCacheOperation}, {@link GetOperation}
+     */
+    public void performOperation(MapOperation operation);
 
 }

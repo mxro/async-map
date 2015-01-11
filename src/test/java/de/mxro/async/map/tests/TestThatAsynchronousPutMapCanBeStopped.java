69792889cb91bb34dac492361841c1f2cc55a87d
mxro <mxro@nowhere.com>
2015-01-11 11:44:10 +1100

    Moving promise implementation into seperate project Autocommit Sun Jan 11 11:44:10 EST 2015

--- src/test/java/de/mxro/async/map/tests/TestThatAsynchronousPutMapCanBeStopped.java
+++ src/test/java/de/mxro/async/map/tests/TestThatAsynchronousPutMapCanBeStopped.java
@@ -1,50 +1,23 @@
 package de.mxro.async.map.tests;
 
-import de.mxro.async.Async;
-import de.mxro.async.Deferred;
-import de.mxro.async.callbacks.SimpleCallback;
-import de.mxro.async.callbacks.ValueCallback;
-import de.mxro.async.jre.AsyncJre;
-import de.mxro.async.map.AsyncMap;
-import de.mxro.async.map.AsyncMaps;
-import de.mxro.concurrency.jre.JreConcurrency;
-import de.mxro.fn.Success;
 import org.junit.Test;
 
 @SuppressWarnings("all")
 public class TestThatAsynchronousPutMapCanBeStopped {
   @Test
   public void test() {
-    JreConcurrency _jreConcurrency = new JreConcurrency();
-    AsyncMap<String, String> _hashMap = AsyncMaps.<String, String>hashMap();
-    final AsyncMap<String, String> map = AsyncMaps.<String, String>enforceAsynchronousPut(10, _jreConcurrency, _hashMap);
-    final Deferred<Success> _function = new Deferred<Success>() {
-      public void apply(final ValueCallback<Success> callback) {
-        SimpleCallback _wrap = Async.wrap(callback);
-        map.start(_wrap);
-      }
-    };
-    AsyncJre.<Success>waitFor(_function);
-    final Deferred<Success> _function_1 = new Deferred<Success>() {
-      public void apply(final ValueCallback<Success> callback) {
-        SimpleCallback _wrap = Async.wrap(callback);
-        map.put("1", "one", _wrap);
-      }
-    };
-    AsyncJre.<Success>waitFor(_function_1);
-    final Deferred<Success> _function_2 = new Deferred<Success>() {
-      public void apply(final ValueCallback<Success> callback) {
-        SimpleCallback _wrap = Async.wrap(callback);
-        map.put("2", "two", _wrap);
-      }
-    };
-    AsyncJre.<Success>waitFor(_function_2);
-    final Deferred<Success> _function_3 = new Deferred<Success>() {
-      public void apply(final ValueCallback<Success> callback) {
-        SimpleCallback _wrap = Async.wrap(callback);
-        map.stop(_wrap);
-      }
-    };
-    AsyncJre.<Success>waitFor(_function_3);
+    throw new Error("Unresolved compilation problems:"
+      + "\nAmbiguous feature call.\nThe methods\n\twrap(ValueCallback<Success>) in Async and\n\t<T> wrap(SimpleCallback) in Async\nboth match."
+      + "\nAmbiguous feature call.\nThe methods\n\twrap(ValueCallback<Success>) in Async and\n\t<T> wrap(SimpleCallback) in Async\nboth match."
+      + "\nAmbiguous feature call.\nThe methods\n\twrap(ValueCallback<Success>) in Async and\n\t<T> wrap(SimpleCallback) in Async\nboth match."
+      + "\nAmbiguous feature call.\nThe methods\n\twrap(ValueCallback<Success>) in Async and\n\t<T> wrap(SimpleCallback) in Async\nboth match."
+      + "\nType mismatch: cannot convert from (ValueCallback<Success>)=>void to Operation<Object>"
+      + "\nType mismatch: cannot convert from (ValueCallback<Success>)=>void to Operation<Object>"
+      + "\nType mismatch: cannot convert from (ValueCallback<Success>)=>void to Operation<Object>"
+      + "\nType mismatch: cannot convert from (ValueCallback<Success>)=>void to Operation<Object>"
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context."
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context."
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context."
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context.");
   }
 }
